afab1ef796d461cc682c3ba8ef7962d1f8ffcb40
Dreary Lisper <drearylisper@gmail.com>
2021-07-12 14:41:32 +0100

    [dvd] Add movements and border detection

--- dvd/sketch.lisp
+++ dvd/sketch.lisp
@@ -4,8 +4,15 @@
 
 (in-package :sketches)
 
-(defsketch dvd ((width 1024) (height 576))
+(defsketch dvd ((width 1024) (height 576) (x 0) (y 0) (dx 3) (dy 2) (logo-width 160) (logo-height 90))
   (background +black+)
-  (image (load-resource "logo.png") 0 0 160 90))
+  (image (load-resource "logo.png") x y logo-width logo-height)
+  (setf x (+ x dx))
+  (setf y (+ y dy))
+  (when (or (<= x 0) (>= x (- width logo-width)))
+    (setf dx (- dx)))
+  (when (or (<= y 0) (>= y (- height logo-height)))
+    (setf dy (- dy)))
+  )
 
 (make-instance 'dvd)
